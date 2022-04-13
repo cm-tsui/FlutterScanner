c0d57417cbe3b15f9bea2eff4737f131d74ac66f
Joe Tsui <cmtsui@gmail.com>
2022-04-13 23:11:42 +0800

    Add folder and file

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_scanner/qr_code_scanner.dart';
 
-
 void main() => runApp(const MaterialApp(home: MyHome()));
 
 class MyHome extends StatelessWidget {
